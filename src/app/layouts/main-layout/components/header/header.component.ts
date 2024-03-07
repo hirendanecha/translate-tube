188206d61700f44d0057e315f45bb19c4bad98c8
krp-opash <krp.opash@gmail.com>
2024-03-07 10:54:54 +0530

    replace changes

--- src/app/layouts/main-layout/components/header/header.component.ts
+++ src/app/layouts/main-layout/components/header/header.component.ts
@@ -54,20 +54,22 @@ export class HeaderComponent {
     private socketService: SocketService
   ) {
     this.originalFavicon = document.querySelector('link[rel="icon"]');
-    this.socketService.socket.on('isReadNotification_ack', (data) => {
-      if (data?.profileId) {
+    if(this.tokenService.getToken()) {
+      this.socketService.socket.on('isReadNotification_ack', (data) => {
+        if (data?.profileId) {
+          this.sharedService.isNotify = false;
+          localStorage.setItem('isRead', data?.isRead);
+          this.originalFavicon.href = '/assets/images/default-profile.jpg';
+        }
+      });
+      const isRead = localStorage.getItem('isRead');
+      if (isRead === 'N') {
+        this.sharedService.isNotify = true;
+      } else {
         this.sharedService.isNotify = false;
-        localStorage.setItem('isRead', data?.isRead);
-        this.originalFavicon.href = '/assets/images/default-profile.jpg';
       }
-    });
-    const isRead = localStorage.getItem('isRead');
-    if (isRead === 'N') {
-      this.sharedService.isNotify = true;
-    } else {
-      this.sharedService.isNotify = false;
+      this.channelId = +localStorage.getItem('channelId');
     }
-    this.channelId = +localStorage.getItem('channelId');
   }
 
   openProfileMenuModal(): void {
