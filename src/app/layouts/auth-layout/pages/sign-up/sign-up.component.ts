800cfc33e02317ad10ffc017fbdb007819ab18c6
prashant <ppo.opash@gmail.com>
2024-06-11 10:09:50 +0530

    Change state input to dropdown

--- src/app/layouts/auth-layout/pages/sign-up/sign-up.component.ts
+++ src/app/layouts/auth-layout/pages/sign-up/sign-up.component.ts
@@ -34,6 +34,8 @@ export class SignUpComponent implements OnInit, AfterViewInit {
   allCountryData: any;
   type = 'danger';
   defaultCountry = 'US';
+  allStateData: any;
+  selectedState = '';
   profilePic: string;
   profileImg: any = {
     file: null,
@@ -266,7 +268,25 @@ export class SignUpComponent implements OnInit, AfterViewInit {
       next: (result) => {
         this.spinner.hide();
         this.allCountryData = result;
-        this.registerForm.get('Zip').enable();
+        this.getAllState(this.defaultCountry)
+      },
+      error: (error) => {
+        this.spinner.hide();
+        console.log(error);
+      },
+    });
+  }
+  onCountryChange(event: Event): void {
+    const target = event.target as HTMLSelectElement;
+    this.getAllState(target.value);
+  }
+  
+  getAllState(selectCountry) {
+    // this.spinner.show();
+    this.customerService.getStateData(selectCountry).subscribe({
+      next: (result) => {
+        this.spinner.hide();
+        this.allStateData = result;
       },
       error: (error) => {
         this.spinner.hide();
