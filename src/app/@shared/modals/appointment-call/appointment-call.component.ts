2a4a54be21461cc22c665b0c49f375dfa07b5064
vatsal <vso.opash@gmail.com>
2024-09-06 17:23:33 +0530

    improve translation data

--- src/app/@shared/modals/appointment-call/appointment-call.component.ts
+++ src/app/@shared/modals/appointment-call/appointment-call.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { ActivatedRoute, Router } from '@angular/router';
+import { SpeechRecognitionService } from '../../services/speech-recognition.service';
+import { SocketService } from '../../services/socket.service';
 
 declare var JitsiMeetExternalAPI: any;
 
@@ -9,21 +11,33 @@ declare var JitsiMeetExternalAPI: any;
   templateUrl: './appointment-call.component.html',
   styleUrls: ['./appointment-call.component.scss'],
 })
-export class AppointmentCallComponent implements OnInit {
+export class AppointmentCallComponent implements OnInit, AfterViewInit {
   appointmentCall: SafeResourceUrl;
-  domain: string = '.meetfacetime.tube';
+  domain: string = 'meet.facetime.tube';
   options: any;
   api: any;
+  transcriptionLog: any[] = [];
+  transcriptText: string = '';
+  appointmentURLCall: string;
 
-  constructor(private route: ActivatedRoute, private sanitizer: DomSanitizer, private router: Router) { }
+  constructor(
+    private route: ActivatedRoute,
+    private sanitizer: DomSanitizer,
+    private router: Router,
+    private socketService: SocketService,
+    private speechRecognitionService: SpeechRecognitionService,
+  ) {}
+
   ngOnInit(): void {
-    const appointmentURLCall = this.route.snapshot['_routerState'].url.split('/appointment-call/')[1];
-    console.log(appointmentURLCall);
+    this.appointmentURLCall =
+      this.route.snapshot['_routerState'].url.split('/appointment-call/')[1];
+    // console.log(this.appointmentURLCall);
     this.appointmentCall = this.sanitizer.bypassSecurityTrustResourceUrl(
-    'https://meet.facetime.tube/' + appointmentURLCall
-  );
+      'https://meet.facetime.tube/' + this.appointmentURLCall
+    );
+
     this.options = {
-      roomName: appointmentURLCall,
+      roomName: this.appointmentURLCall,
       parentNode: document.querySelector('#meet'),
       configOverwrite: {
         prejoinPageEnabled: false,
@@ -37,9 +51,61 @@ export class AppointmentCallComponent implements OnInit {
     };
 
     const api = new JitsiMeetExternalAPI(this.domain, this.options);
+    this.speechRecognitionService.start();
+    // this.speechRecognitionService.setLanguage(navigator.language || 'en-US');
+
     api.on('readyToClose', () => {
-      this.router.navigate(['/home']).then(() => {
-      });
+      this.speechRecognitionService.stop();
+      this.router.navigate(['/home']).then(() => {});
     });
+    // api.addToolbarButton(
+    //   'translationButton',
+    //   'Translate',
+    //   () => {
+    //     this.onTranslate();
+    //   },
+    //   'custom-translate-button',
+    //   'Click to translate',
+    //   true
+    // );
+  }
+
+  ngAfterViewInit(): void {
+    if (this.socketService?.socket && !this.socketService?.socket?.connected) {
+      this.socketService?.connect();
+    }
+    console.log(this.appointmentURLCall);
+    this.socketService?.socket?.emit('join', this.appointmentURLCall);
+    this.configureSpeechRecognition();
+    this.socketService.socket?.on('translations', (res) => {
+    let timeoutId: any;
+      if (timeoutId) {
+        clearTimeout(timeoutId);
+      }
+      this.transcriptText = res?.data[0]?.translatedText;
+      console.log(this.transcriptText);
+      timeoutId = setTimeout(() => {
+        this.transcriptText = '';
+      }, 1500);
+      console.log(res);
+    });
+  }
+
+  configureSpeechRecognition() {
+    this.speechRecognitionService.recognition.onresult = (event: any) => {
+      const transcripts = Array.from(event.results).map(
+        (result: any) => result[0].transcript
+      );
+      const currentTranscript = transcripts[transcripts.length - 1];
+      const reqObj = {
+        callId: this.appointmentURLCall,
+        translateText: currentTranscript,
+        translateLanguage: 'en-US',
+      };
+      console.log(currentTranscript);
+      this.socketService.translationSocketService(reqObj, (data) => {
+        console.log(data);
+      });
+    };
   }
 }
